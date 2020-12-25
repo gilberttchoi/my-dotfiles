08fa36474ca3ab5ac1df9d92cdfd81a5b51afaeb
Gilbert Tchoi <17569832+gilberttchoi@users.noreply.github.com>
2020-12-25 21:08:41 +0100

    add oh-my-zsh setup

---
+++ shell/ohmyzshsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+
+mkdir $ZSH_CUSTOM/plugins/poetry
+poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
--- shell/setup.sh
+++ shell/setup.sh
@@ -1,1 +1,3 @@
 sudo chsh -s $(which zsh)
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
